841daccd83e5ae10c9c9eae1e5f3ab19b0d9372d
Damian Arya <dahakcyrus@yahoo.com>
2020-03-27 00:12:36 -0600

    removed unessesary txt line 120 in php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
 				<div class="container">
 					<img src="images/stunning.jpg" alt="captain picard" />
 					<h2>A Day in the Life...</h2>
-					<p>Engage Some days you get the bear, and some days the bear gets you. I'd like to think that I haven't changed those things, sir. Did you come here for something in particular or just general Riker-bashing? That might've been one of the shortest assignments in the history of Starfleet.</p>
+					<p>Engage Some days you get the bear, and some days the bear! gets you. I'd like to think that I haven't changed those things, sir. Did you come here for something in particular or just general Riker-bashing? That might've been one of the shortest assignments in the history of Starfleet.</p>
 					<p>Hiss and stare at nothing! then run suddenly away. Bare fangs at toy run hide in litter box until treats are fed toy mouse squeak roll over yet stare at the wall, play with food and get confused by dust. Chase dog then run away drink water out of the faucet for find a way to fit in tiny box. <span>Groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked!</span></p>
 					<div class="clearfix"></div>
 				</div>
@@ -117,7 +117,7 @@
 			</section>
 <!--this is for a commit yet again!-->
 		</div><!--/.sfooter-content-->
-<!--this is an ungraded in class and homework assignment for playing with and learning css!!-->
+<!--this is an ungraded in class and homework assignment for playing with and learning css -->
 		<footer>
 			<div class="container">
 				<h4>Best viewed with Netscape Navigator. Proudly hosted by GeoCities.</h4>
